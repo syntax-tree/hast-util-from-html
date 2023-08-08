7162520fbe4f700f753936ab02e4dba05648c2a9
Titus Wormer <tituswormer@gmail.com>
2023-08-08 17:55:37 +0200

    Fix to register data from `hast-util-from-parse5`

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /**
+ * @typedef {import('hast-util-from-parse5')} DoNotTouchItRegistersData
+ *
  * @typedef {import('./lib/index.js').ErrorCode} ErrorCode
  * @typedef {import('./lib/index.js').ErrorSeverity} ErrorSeverity
  * @typedef {import('./lib/index.js').OnError} OnError
